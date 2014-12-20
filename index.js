567b2821b6519cd102758bb94101ff5a22f95572
Brent Jackson <jacksonblack@gmail.com>
2014-12-20 13:42:06 -0500

    Aggregates

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 
-//var css = require('css');
 var postcss = require('postcss');
 var declarations = require('./lib/declarations');
 var selectors = require('./lib/selectors');
-//var aggregates = require('./lib/aggregates');
-//var rules = require('./lib/rules');
+var aggregates = require('./lib/aggregates');
+var rules = require('./lib/rules');
 var size = require('./lib/size');
 
 module.exports = function(string, options) {
@@ -20,8 +19,8 @@ module.exports = function(string, options) {
 
   result.selectors = selectors(obj);
   result.declarations = declarations(obj);
-  //result.rules = rules(ast);
-  //result.aggregates = aggregates(result);
+  result.rules = rules(obj);
+  result.aggregates = aggregates(result);
 
   return result;
 
