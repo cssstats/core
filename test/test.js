e7c6258164ffa52250da4d021433a3722b6063f1
John Otander <johnotander@gmail.com>
2015-01-26 16:57:27 -0700

    Test base stats, averages, and aggregates

--- test/test.js
+++ test/test.js
@@ -3,19 +3,24 @@ var assert = require('assert');
 var cssstats = require('..');
 
 describe('css-statistics', function() {
+  var stats;
 
-  it('should be able to parse css and produce stats', function() {
-    ['basscss', 'small', 'font-awesome', 'gridio', 'gridio-national-light'].forEach(function(stylesheet) {
-      renderJson(stylesheet);
+  before(function() {
+    stats = cssstats(fixture('small'));
+  });
+
+  describe('base stats', function() {
+
+    it('should calculate the correct file size', function() {
+      assert.equal(stats.size, 530);
+    });
+
+    it('should calculate the correct gzipped file size', function() {
+      assert.equal(stats.gzipSize, 205);
     });
   });
 
   describe('averages', function() {
-    var stats;
-
-    before(function() {
-      stats = cssstats(fixture('small'));
-    });
 
     it('should correctly count specificity stats', function() {
       assert.equal(stats.averages.specificity, 5.5);
@@ -24,10 +29,30 @@ describe('css-statistics', function() {
     it('should correctly count rule size stats', function() {
       assert.equal(stats.averages.ruleSize, 2.75);
     });
+  });
+
+  describe('aggregates', function() {
+
+    it('should correclty count declarations', function() {
+      assert.equal(stats.aggregates.declarations, 11);
+    });
+
+    it('should correclty count selectors', function() {
+      assert.equal(stats.aggregates.selectors, 4);
+    });
+  });
+
+  describe('declarations', function() {
 
     it('should correclty count vendor prefixes', function() {
       assert.equal(stats.declarations.vendorPrefixCount, 4);
-    })
+    });
+  });
+
+  it('should be able to parse css and produce stats', function() {
+    ['basscss', 'small', 'font-awesome', 'gridio', 'gridio-national-light'].forEach(function(stylesheet) {
+      renderJson(stylesheet);
+    });
   });
 });
 
