b9d5d1029c95a9eba6ad6bc5640e7631d1150a1a
jxnblk <jacksonblack@gmail.com>
2015-07-28 00:35:40 -0400

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,146 +1,164 @@
-var fs = require('fs');
-var assert = require('assert');
-var postcss = require('postcss');
-var cssstats = require('..');
+
+var fs = require('fs')
+var assert = require('assert')
+var postcss = require('postcss')
+var cssstats = require('..')
 
 describe('css-statistics', function() {
-  var stats;
+  var stats
 
   before(function() {
-    stats = cssstats(fixture('small'));
-  });
+    stats = cssstats(fixture('small'))
+  })
 
-  describe('AST arguments', function() {
+  describe('PostCSS plugin', function() {
 
-    it('should be handled correctly', function() {
-      var ast = postcss.parse(fixture('small'));
-      var astStats = cssstats(ast);
-      assert.deepEqual(astStats.aggregates, stats.aggregates);
-    });
-  });
+    it('should be handled correctly', function(done) {
+      postcss()
+        .use(cssstats())
+        .process(fixture('small'))
+        .then(function (result) {
+          var pluginStats = result.messages[0].stats
+          assert.deepEqual(pluginStats.aggregates, stats.aggregates)
+          done()
+        })
+    })
+  })
 
   describe('base stats', function() {
 
     it('should calculate the correct file size', function() {
-      assert.equal(stats.size, 827);
-    });
+      assert.equal(stats.size, 827)
+    })
 
     it('should calculate the correct gzipped file size', function() {
-      assert.equal(stats.gzipSize, 336);
-    });
-  });
+      assert.equal(stats.gzipSize, 336)
+    })
+  })
 
   describe('averages', function() {
 
     it('should correctly count specificity stats', function() {
-      assert.equal(stats.averages.specificity, 20.272727272727273);
-    });
+      assert.equal(stats.selectors.specificity.average, 20.272727272727273)
+    })
 
     it('should correctly count rule size stats', function() {
-      assert.equal(stats.averages.ruleSize, 2);
-    });
-  });
+      assert.equal(stats.rules.size.average, 2)
+    })
+  })
 
   describe('aggregates', function() {
 
     it('should correctly count declarations', function() {
-      assert.equal(stats.aggregates.declarations, 20);
-    });
+      assert.equal(stats.declarations.total, 20)
+    })
 
     it('should correctly count selectors', function() {
-      assert.equal(stats.aggregates.selectors, 11);
-    });
+      assert.equal(stats.selectors.total, 11)
+    })
 
     it('should correctly count the number of id selectors', function() {
-      assert.equal(stats.aggregates.idSelectors, 1);
-    });
+      assert.equal(stats.selectors.id, 1)
+    })
 
     it('should correctly count the number of class selectors', function() {
-      assert.equal(stats.aggregates.classSelectors, 8);
-    });
+      assert.equal(stats.selectors.class, 8)
+    })
 
     it('should correctly count the number of pseudo element selectors', function() {
-      assert.equal(stats.aggregates.pseudoElementSelectors, 1);
-    });
+      assert.equal(stats.selectors.pseudoElement, 1)
+    })
 
     it('should correctly count the number of pseudo class selectors', function() {
-      assert.equal(stats.aggregates.pseudoClassSelectors, 3);
-    });
+      assert.equal(stats.selectors.pseudoClass, 3)
+    })
 
     it('should correctly aggregate the repeated selectors', function() {
-      assert.deepEqual(stats.aggregates.repeatedSelectors, ['.red']);
-    });
-  });
+      assert.deepEqual(stats.selectors.repeated, ['.red'])
+    })
+  })
 
   describe('declarations', function() {
 
     it('should correctly count vendor prefixes', function() {
-      assert.equal(stats.declarations.vendorPrefixCount, 5);
-    });
+      assert.equal(stats.declarations.vendorPrefix, 5)
+    })
 
     it('should correctly count important values', function() {
-      assert.equal(stats.declarations.importantCount, 2);
-    });
+      assert.equal(stats.declarations.important, 2)
+    })
 
-    it('should correctly count the number of unique declarations', function() {
-      assert.equal(stats.declarations.uniqueDeclarationsCount, 19);
-    });
+    // Deprecate in favor of presentation-side uniquing
+    // it('should correctly count the number of unique declarations', function() {
+    //   assert.equal(stats.declarations.uniqueDeclarationsCount, 19)
+    // })
 
     it('should correctly count the number of declarations that reset properties', function() {
-      assert.deepEqual(stats.declarations.propertyResetDeclarations, {'margin': 1, 'marginBottom': 1});
-    });
-  });
+      assert.deepEqual(stats.declarations.resets, {'margin': 1, 'margin-bottom': 1})
+    })
+  })
 
   describe('keyframes', function() {
-    var keyframeStats;
+    var keyframeStats
 
     before(function() {
-      keyframeStats = cssstats(fixture('keyframes'));
-    });
+      keyframeStats = cssstats(fixture('keyframes'))
+    })
 
     it('should correctly get statistics for CSS in, and after, a keyframe', function() {
-      assert.equal(keyframeStats.aggregates.color.total, 5);
-      assert.equal(keyframeStats.aggregates.color.unique, 4);
-    });
-  });
+      assert.equal(keyframeStats.declarations.properties.color.length, 5)
+      // assert.equal(keyframeStats.aggregates.color.unique, 4)
+    })
+  })
 
-  it('should be able to parse css and produce stats', function() {
-    ['basscss', 'small', 'font-awesome', 'gridio', 'gridio-national-light'].forEach(function(stylesheet) {
-      renderJson(stylesheet);
-    });
-  });
-});
+  it('should be able to parse css and produce stats', function(done) {
+    [
+      'basscss',
+      'small',
+      // 'font-awesome',
+      'gridio',
+      'gridio-national-light'
+    ].forEach(function(stylesheet) {
+      renderJson(stylesheet)
+    })
+    done()
+  })
+})
 
+// This seems like something better suited for the app, not the core
+/*
 describe('font shorthand property', function() {
-  var stats;
+  var stats
 
   before(function() {
-    stats = cssstats(fixture('font-shorthand'));
-  });
+    stats = cssstats(fixture('font-shorthand'))
+  })
 
   it('should be able to grab the font-size declaration', function() {
-    assert.equal(stats.aggregates.fontSize.total, 2);
-  });
+    assert.equal(stats.declarations.properties['font-size'].length, 2)
+  })
 
   it('should be able to grab the font-family declaration', function() {
-    assert.equal(stats.aggregates.fontFamily.total, 1);
-  });
+    assert.equal(stats.declarations.properties['font-family'].length, 1)
+  })
 
   it('should be able to grab the font-weight declaration', function() {
-    assert.equal(stats.aggregates.fontWeight.total, 1);
-  });
+    assert.equal(stats.declarations.properties['font-weight'].length, 1)
+  })
 
   it('should be able to grab the font-style declaration', function() {
-    assert.equal(stats.aggregates.fontStyle.total, 1);
-  });
-});
+    assert.equal(stats.aggregates.fontStyle.total, 1)
+  })
+})
+*/
 
 function fixture(name) {
-  return fs.readFileSync('test/fixtures/' + name + '.css', 'utf8').toString().trim();
+  return fs.readFileSync('test/fixtures/' + name + '.css', 'utf8').toString().trim()
 }
 
 function renderJson(filename) {
-  var css = fixture(filename);
-  var obj = cssstats(css);
-  fs.writeFileSync('./test/results/' + filename + '.json', JSON.stringify(obj, null, 2));
+  var css = fixture(filename)
+  var obj = cssstats(css)
+  fs.writeFileSync('./test/results/' + filename + '.json', JSON.stringify(obj, null, 2))
 }
+
