4b433a0c36e1268fc35fc2250fdd482ef0d9b615
jxnblk <jacksonblack@gmail.com>
2015-07-27 21:27:41 -0400

    Rename results

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
-var _ = require('lodash');
-var postcss = require('postcss');
-var gzipSize = require('gzip-size');
-var declarations = require('./lib/declarations');
-var selectors = require('./lib/selectors');
-var aggregates = require('./lib/aggregates');
-var rules = require('./lib/rules');
-var size = require('./lib/size');
+
+var _ = require('lodash')
+var postcss = require('postcss')
+var gzipSize = require('gzip-size')
+var declarations = require('./lib/declarations')
+var selectors = require('./lib/selectors')
+var aggregates = require('./lib/aggregates')
+var rules = require('./lib/rules')
+var size = require('./lib/size')
 
 module.exports = function(cssStringOrAST, options) {
-  options = options || {};
-  options.safe = options.safe || true;
 
-  var obj;
-  var string;
-  var result = {};
+  options = options || {}
+  options.safe = options.safe || true
+
+  var obj
+  var string
+  var results = {}
 
   if (_.isString(cssStringOrAST)) {
-    obj = postcss.parse(cssStringOrAST, options);
-    string = cssStringOrAST;
+    obj = postcss.parse(cssStringOrAST, options)
+    string = cssStringOrAST
   } else if (_.isObject(cssStringOrAST)) {
-    obj = cssStringOrAST;
-    string = obj.toString();
+    obj = cssStringOrAST
+    string = obj.toString()
   } else {
-    throw new TypeError('cssstats expects a string or PostCSS AST');
+    throw new TypeError('cssstats expects a string or PostCSS AST')
   }
 
-  if (!obj) { return false; }
+  if (!obj) { return false }
 
-  result.averages = {};
-  result.size = size(string);
-  result.gzipSize = gzipSize.sync(string);
+  results.averages = {}
+  results.size = size(string)
+  results.gzipSize = gzipSize.sync(string)
 
-  var selectorStats = selectors(obj);
-  result.selectors = selectorStats.selectors;
-  result.averages.specificity = selectorStats.averageSpecificity;
+  var selectorStats = selectors(obj)
+  results.selectors = selectorStats.selectors
+  results.averages.specificity = selectorStats.averageSpecificity
 
-  var ruleStats = rules(obj);
-  result.rules = ruleStats.rules;
-  result.averages.ruleSize = ruleStats.averageRuleSize;
+  var ruleStats = rules(obj)
+  results.rules = ruleStats.rules
+  results.averages.ruleSize = ruleStats.averageRuleSize
 
-  result.declarations = declarations(obj);
-  result.aggregates = aggregates(result);
+  results.declarations = declarations(obj)
+  results.aggregates = aggregates(results)
 
-  result.aggregates.idSelectors = selectorStats.idSelectors;
-  result.aggregates.classSelectors = selectorStats.classSelectors;
-  result.aggregates.repeatedSelectors = selectorStats.repeatedSelectors;
-  result.aggregates.pseudoClassSelectors = selectorStats.pseudoClassSelectors;
-  result.aggregates.pseudoElementSelectors = selectorStats.pseudoElementSelectors;
+  results.aggregates.idSelectors = selectorStats.idSelectors
+  results.aggregates.classSelectors = selectorStats.classSelectors
+  results.aggregates.repeatedSelectors = selectorStats.repeatedSelectors
+  results.aggregates.pseudoClassSelectors = selectorStats.pseudoClassSelectors
+  results.aggregates.pseudoElementSelectors = selectorStats.pseudoElementSelectors
 
-  return result;
-};
+  return results
+
+}
