82426734d264d6edef1609b937f68399928d3d69
jxnblk <jacksonblack@gmail.com>
2015-08-05 10:47:55 -0400

    Update tests

--- test/test.js
+++ test/test.js
@@ -27,63 +27,83 @@ describe('css-statistics', function () {
     })
   })
 
-  describe('base stats', function () {
+  describe('size stats', function () {
     it('should calculate the correct file size', function () {
-      assert.equal(stats.size, 827)
+      assert.equal(stats.size, 885)
     })
 
     it('should calculate the correct gzipped file size', function () {
-      assert.equal(stats.gzipSize, 336)
+      assert.equal(stats.gzipSize, 366)
     })
   })
 
-  describe('averages', function () {
-    it('should correctly count specificity stats', function () {
-      assert.equal(stats.selectors.specificity.average, 20.272727272727273)
+  describe('rules', function () {
+    it('should count the total number of rules', function () {
+      assert.equal(stats.rules.total, 12)
     })
 
-    it('should correctly count rule size stats', function () {
-      assert.equal(stats.rules.size.average, 2)
+    it('should correctly calculate max rule size', function () {
+      assert.equal(stats.rules.size.max, 4)
+    })
+
+    it('should correctly calculate average rule size', function () {
+      assert.equal(stats.rules.size.average, 1.8333333333333333)
+    })
+
+    it('should return a rule size graph', function () {
+      assert.deepEqual(stats.rules.size.graph, [ 1, 1, 3, 1, 2, 2, 1, 1, 4, 4, 1, 1 ])
     })
   })
 
-  describe('totals', function () {
-    it('should correctly count declarations', function () {
-      assert.equal(stats.declarations.total, 20)
-    })
-
-    it('should correctly count selectors', function () {
-      assert.equal(stats.selectors.total, 11)
+  describe('selectors', function () {
+    it('should correctly count total selectors', function () {
+      assert.equal(stats.selectors.total, 13)
     })
 
     it('should correctly count the number of id selectors', function () {
       assert.equal(stats.selectors.id, 1)
     })
 
-    it('should correctly count the number of class selectors', function () {
-      assert.equal(stats.selectors.class, 8)
+    it('should correctly count the number of type selectors', function () {
+      assert.equal(stats.selectors.type, 1)
     })
 
-    it('should correctly count the number of pseudo element selectors', function () {
-      assert.equal(stats.selectors.pseudoElement, 1)
+    it('should correctly count the number of class selectors', function () {
+      assert.equal(stats.selectors.class, 9)
     })
 
     it('should correctly count the number of pseudo class selectors', function () {
       assert.equal(stats.selectors.pseudoClass, 3)
     })
 
-    it('should correctly aggregate the repeated selectors', function () {
-      assert.deepEqual(stats.selectors.getRepeatedValues(), ['.red'])
+    it('should correctly count the number of pseudo element selectors', function () {
+      assert.equal(stats.selectors.pseudoElement, 1)
+    })
+
+    it('should return an array of selectors', function () {
+      assert.equal(stats.selectors.values.length > 0, true)
+    })
+
+    it('should correctly calculate max specificity', function () {
+      assert.equal(stats.selectors.specificity.max, 100)
+    })
+
+    it('should correctly calculate average specificity', function () {
+      assert.equal(stats.selectors.specificity.average, 18)
     })
   })
 
   describe('declarations', function () {
-    it('should correctly count vendor prefixes', function () {
-      assert.equal(stats.declarations.getVendorPrefixed().length, 5)
+    it('should correctly count total declarations', function () {
+      assert.equal(stats.declarations.total, 22)
     })
 
     it('should correctly count important values', function () {
       assert.equal(stats.declarations.important.length, 2)
+    })
+
+    it('should return a properties object', function () {
+      assert.equal(Object.keys(stats.declarations.properties).length > 0, true)
     })
   })
 
@@ -101,17 +121,21 @@ describe('css-statistics', function () {
 
   describe('selector methods', function () {
     it('should generate a specificity graph', function () {
-      assert.deepEqual(stats.selectors.getSpecificityGraph(), [ 10, 100, 10, 10, 11, 30, 10, 20, 20, 1, 1 ])
+      assert.equal(stats.selectors.getSpecificityGraph().length > 0, true)
     })
 
     it('should return a sorted specificity array', function () {
-      assert.deepEqual(stats.selectors.getSortedSpecificity(), [ { selector: '#foo', specificity: 100 }, { selector: '.sm-tomato:first-child:last-child', specificity: 30 }, { selector: '.box:first-child', specificity: 20 }, { selector: '.box:last-child', specificity: 20 }, { selector: '.sm-tomato::after', specificity: 11 }, { selector: '.red', specificity: 10 }, { selector: '.box', specificity: 10 }, { selector: '.sm-tomato', specificity: 10 }, { selector: '.red', specificity: 10 }, { selector: '0%', specificity: 1 }, { selector: '100%', specificity: 1 } ])
+      assert.deepEqual(stats.selectors.getSortedSpecificity(), [ { selector: '#foo', specificity: 100 }, { selector: '.sm-tomato:first-child:last-child', specificity: 30 }, { selector: '.box:first-child', specificity: 20 }, { selector: '.box:last-child', specificity: 20 }, { selector: '.sm-tomato::after', specificity: 11 }, { selector: '.box', specificity: 10 }, { selector: '.sm-tomato', specificity: 10 }, { selector: '.red', specificity: 10 }, { selector: '.red', specificity: 10 }, { selector: '.georgia', specificity: 10 }, { selector: '0%', specificity: 1 }, { selector: '100%', specificity: 1 }, { selector: 'header', specificity: 1 } ])
+    })
+
+    it('should return repeated selectors', function () {
+      assert.deepEqual(stats.selectors.getRepeatedValues(), [ '.red' ])
     })
   })
 
   describe('declaration methods', function () {
     it('should correctly count the number of declarations that reset properties', function () {
-      assert.deepEqual(stats.declarations.getPropertyResets(), {'margin': 1, 'margin-bottom': 1})
+      assert.deepEqual(stats.declarations.getPropertyResets(), {'margin': 1, 'padding': 1, 'margin-bottom': 1})
     })
 
     it('should correctly count the number of unique colors', function () {
@@ -122,12 +146,16 @@ describe('css-statistics', function () {
       assert.equal(stats.declarations.getPropertyValueCount('color', 'red'), 2)
     })
 
+    it('should get count vendor prefixes', function () {
+      assert.equal(stats.declarations.getVendorPrefixed().length, 5)
+    })
+
     it('should get all font sizes', function () {
-      assert.equal(stats.declarations.getAllFontSizes().length, 0)
+      assert.equal(stats.declarations.getAllFontSizes().length, 1)
     })
 
     it('should get all font families', function () {
-      assert.equal(stats.declarations.getAllFontFamilies().length, 0)
+      assert.equal(stats.declarations.getAllFontFamilies().length, 1)
     })
   })
 
@@ -135,7 +163,7 @@ describe('css-statistics', function () {
     [
       'basscss',
       'small',
-      // 'font-awesome',
+      'font-awesome',
       'gridio',
       'gridio-national-light'
     ].forEach(function (stylesheet) {
@@ -145,19 +173,11 @@ describe('css-statistics', function () {
   })
 })
 
-describe('cssstats lite', function () {
+describe('cssstats no media queries', function () {
   var stats
 
   before(function () {
-    stats = cssstats(fixture('small'), { lite: true, mediaQueries: false })
-  })
-
-  it('should not contain rulesize graph', function () {
-    assert.equal(stats.rules.size.graph, null)
-  })
-
-  it('should not contain selector values', function () {
-    assert.equal(stats.selectors.values, null)
+    stats = cssstats(fixture('small'), { mediaQueries: false })
   })
 
   it('should not contain media query contents', function () {
